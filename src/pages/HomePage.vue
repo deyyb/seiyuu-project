8011f31ff6e1e85b88deff60aef6ad054e2351ed
deyyb <davebajuyo1@gmail.com>
2024-10-15 09:13:55 +0800

    added infinity loading on home screen

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,22 @@
 <template>
   <q-page padding>
     <!-- content -->
-    <div class="q-pa-md row items-start q-gutter-md">
-      <div v-for="seiyuu in seiyuus" :key="seiyuu.seiyuuId">
-        <seiyuu-card
-          :seiyuu-name="seiyuu.seiyuuName"
-          :seiyuu-id="seiyuu.seiyuuId"
-        />
+    <q-infinite-scroll @load="onLoad" :offset="250">
+      <div class="q-pa-md row items-start q-gutter-md">
+        <div v-for="seiyuu in seiyuus" :key="seiyuu.seiyuuId">
+          <seiyuu-card
+            :seiyuu-name="seiyuu.seiyuuName"
+            :seiyuu-id="seiyuu.seiyuuId"
+          />
+        </div>
       </div>
-    </div>
+      <template v-slot:loading>
+        <div class="row justify-center q-my-md">
+          <q-spinner-dots color="primary" size="40px" />
+        </div>
+      </template>
+    </q-infinite-scroll>
+    <q-card v-show="end">No more data!</q-card>
   </q-page>
 </template>
 
@@ -25,16 +33,34 @@ export default {
       seiyuuName: "",
       link: ``,
       seiyuus: [],
+      page: 0,
+      end: false,
     };
   },
   async mounted() {
     try {
       const response = await this.$api.get(`seiyuu/allSeiyuu/0`);
-      this.seiyuus = response.data;
+      this.seiyuus.push(...response.data);
       console.log(response.data);
     } catch (error) {
       console.log(error);
     }
+  },
+  methods: {
+    onLoad(index, done) {
+      if (this.page < 150) {
+        setTimeout(async () => {
+          const response = await this.$api.get(
+            `seiyuu/allSeiyuu/${this.page++}`
+          );
+          this.seiyuus.push(...response.data);
+          done();
+        }, 2000);
+      } else {
+        this.end = true;
+        done();
+      }
+    },
   },
 };
 </script>
