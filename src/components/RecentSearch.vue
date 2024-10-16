7ebe05112f0e52de44a327c30bb593e10ec17218
deyyb <davebajuyo1@gmail.com>
2024-10-16 14:23:19 +0800

    added recent search history component

---
+++ src/components/RecentSearch.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row q-ma-xs" style="width: 100%">
+    <div class="col-2" style="margin-right: 10px">
+      <div class="flex justify-start q-ma-sm">
+        <q-avatar size="lg">
+          <img :src="iconPic" />
+        </q-avatar>
+      </div>
+    </div>
+    <div class="col-7 flex items-center">
+      <div>
+        <h6 style="color: black; margin: 0 0 0 0">{{ name }}</h6>
+      </div>
+      <div v-if="show">
+        <q-badge color="blue" rounded class="q-mr-sm" />
+        <span style="color: gray">{{ newValue }} new</span>
+      </div>
+      <div v-if="showPlus">
+        <q-badge color="blue" rounded class="q-mr-sm" />
+        <span style="color: gray">9+ new</span>
+      </div>
+    </div>
+    <div class="col-2 flex justify-end">
+      <div class="flex justify-end">
+        <div style="color: black" class="q-ma-xs">
+          <q-avatar size="lg" class="q-ma-xs">
+            <q-icon name="clear"></q-icon>
+          </q-avatar>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecentSearch",
+  props: {
+    iconPic: String,
+    name: String,
+    newValue: String,
+  },
+  data() {
+    return {
+      show: false,
+      showPlus: false,
+      new: Number(this.newValue),
+    };
+  },
+  mounted() {
+    if (this.new > 0 && this.new < 9) {
+      this.show = true;
+    } else if (this.new > 9) {
+      this.showPlus = true;
+    }
+  },
+};
+</script>
--- src/components/searchDialog.vue
+++ src/components/searchDialog.vue
@@ -1,29 +1,61 @@
 <template>
-  <q-card style="width: 350px">
+  <q-card style="width: 300px">
     <div class="row q-pa-sm">
       <!-- For logo and search -->
-      <q-avatar size="lg" class="q-ma-xs">
-        <q-icon name="arrow_back" @click="closeSearch" />
-      </q-avatar>
-      <div class="q-ma-xs" style="width: 220px">
-        <q-input
-          :v-model="inputValue"
-          autofocus
-          dense
-          rounded
-          standout="bg-fb"
-          color="black"
-          placeholder="Search Facebook"
-          input-class="text-fb"
-        >
-        </q-input>
+      <div class="row" style="width: 100%">
+        <q-avatar size="lg" class="q-ma-xs">
+          <q-icon name="arrow_back" @click="closeSearch" />
+        </q-avatar>
+        <div class="col-9 q-ma-xs">
+          <div style="width: 100%">
+            <q-input
+              :v-model="inputValue"
+              autofocus
+              dense
+              rounded
+              standout="bg-fb"
+              color="black"
+              placeholder="Search Facebook"
+              input-class="text-fb"
+            >
+            </q-input>
+          </div>
+        </div>
       </div>
+
+      <!-- For Recent -->
+      <div class="row q-ma-xs" style="width: 100%">
+        <div class="col-6">
+          <div class="flex justify-start">
+            <h6 style="color: black; margin: 0 0 0 0">Recent</h6>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="flex justify-end">
+            <div style="color: black" class="q-ma-xs">
+              <a href="#/fb" style="text-decoration: none">edit</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- For the recent searches -->
+      <recent-search
+        icon-pic="public/icons/rui.jpg"
+        name="Rui Kamishiro"
+        new-value="7"
+      />
     </div>
   </q-card>
 </template>
 
 <script>
+import RecentSearch from "./RecentSearch.vue";
+
 export default {
+  components: {
+    RecentSearch,
+  },
   data() {
     return {
       inputValue: "",
