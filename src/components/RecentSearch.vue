0e7759985a3aca0d6f395e9690ce7862e2e713cd
deyyb <davebajuyo1@gmail.com>
2024-10-17 10:06:44 +0800

    added loading and fetching function placeholder for search

--- src/components/RecentSearch.vue
+++ src/components/RecentSearch.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="row q-ma-xs" style="width: 100%">
-    <div class="col-2" style="margin-right: 10px">
-      <div class="flex justify-start q-ma-sm">
-        <q-avatar size="lg">
-          <img :src="iconPic" />
-        </q-avatar>
-      </div>
-    </div>
-    <div class="row col-7 flex items-center">
-      <div class="col-12">
-        <h6 style="color: black; margin: 0 0 0 0">{{ name }}</h6>
-      </div>
-      <div v-if="show">
-        <q-badge color="blue" rounded class="q-mr-sm" />
-        <span style="color: gray">{{ newValue }} new</span>
-      </div>
-      <div v-if="showPlus">
-        <q-badge color="blue" rounded class="q-mr-sm" />
-        <span style="color: gray">9+ new</span>
-      </div>
-    </div>
-    <div class="col-2 flex justify-end">
-      <div class="flex justify-end">
-        <div style="color: black" class="q-ma-xs">
-          <q-avatar size="lg" class="q-ma-xs">
-            <q-icon name="clear"></q-icon>
+  <q-btn flat align="left" class="full-width" no-caps>
+    <div class="row" style="width: 100%">
+      <div class="col-2" style="margin-right: 10px">
+        <div class="flex justify-start q-ma-xs">
+          <q-avatar size="lg">
+            <img :src="iconPic" />
           </q-avatar>
         </div>
       </div>
+      <div class="row col-7 flex items-center">
+        <div class="col-12 flex items-start">
+          <strong style="color: black; margin: 0 0 0 0">{{ name }}</strong>
+        </div>
+        <div v-if="show">
+          <q-badge color="blue" rounded class="q-mr-sm" />
+          <span style="color: gray">{{ newValue }} new</span>
+        </div>
+        <div v-if="showPlus">
+          <q-badge color="blue" rounded class="q-mr-sm" />
+          <span style="color: gray">9+ new</span>
+        </div>
+      </div>
+      <div class="col-2 flex justify-end">
+        <div class="flex justify-end">
+          <div style="color: black">
+            <q-avatar size="lg" class="q-ma-xs">
+              <q-icon name="clear"></q-icon>
+            </q-avatar>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  </q-btn>
 </template>
 
 <script>
--- src/components/searchDialog.vue
+++ src/components/searchDialog.vue
@@ -9,7 +9,7 @@
         <div class="col-9 q-ma-xs">
           <div style="width: 100%">
             <q-input
-              :v-model="inputValue"
+              v-model="inputValue"
               autofocus
               dense
               rounded
@@ -23,29 +23,53 @@
         </div>
       </div>
 
-      <!-- For Recent -->
-      <div class="row q-ma-xs" style="width: 100%">
-        <div class="col-6">
-          <div class="flex justify-start">
-            <h6 style="color: black; margin: 0 0 0 0">Recent</h6>
+      <div v-if="fetching">
+        <q-btn flat align="left" class="full-width" no-caps>
+          <q-avatar rounded size="md" class="q-ma-xs">
+            <q-icon name="search"></q-icon>
+          </q-avatar>
+          <div class="q-pa-xs">
+            <span style="margin: 0 0 0 0">{{ inputValue }}</span>
           </div>
-        </div>
-        <div class="col-6">
-          <div class="flex justify-end">
-            <div style="color: black" class="q-ma-xs">
-              <a href="#/fb" style="text-decoration: none">edit</a>
+        </q-btn>
+      </div>
+
+      <div v-else>
+        <!-- For Recent -->
+        <div class="row q-ma-xs" style="width: 100%">
+          <div class="col-6">
+            <div class="flex justify-start">
+              <h6 style="color: black; margin: 0 0 0 0">Recent</h6>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="flex justify-end">
+              <div style="color: black" class="q-ma-xs">
+                <a href="#/fb" style="text-decoration: none">edit</a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <!-- For the recent searches -->
-      <recent-search
-        v-for="search in searches"
-        :key="search.name"
-        :icon-pic="search.pic"
-        :name="search.name"
-        :new-value="search.new"
+        <!-- For the recent searches -->
+        <recent-search
+          v-for="search in searches"
+          :key="search.name"
+          :icon-pic="search.pic"
+          :name="search.name"
+          :new-value="search.new"
+        />
+      </div>
+    </div>
+
+    <div class="row flex justify-center">
+      <q-circular-progress
+        v-show="loading"
+        indeterminate
+        rounded
+        size="25px"
+        color="blue-grey-11"
+        class="q-ma-md"
       />
     </div>
   </q-card>
@@ -61,6 +85,9 @@ export default {
   data() {
     return {
       inputValue: "",
+      fetching: false,
+      loading: false,
+      timeout: null,
       searches: [
         {
           pic: "public/icons/rui.jpg",
@@ -95,13 +122,33 @@ export default {
       this.$emit("close-search");
       // console.log("closing");
     },
+    fetchSearch() {
+      this.fetching = true;
+      this.loading = false;
+    },
+    handleEmptySearch() {
+      this.fetching = false;
+      this.loading = false;
+    },
   },
-  // watch: (
-  //   inputValue(val) {
-  //     if (!val) {
+  watch: {
+    inputValue(newVal) {
+      // console.log(newVal);
 
-  //     }
-  //   }
-  // )
+      clearTimeout(this.timeout);
+      this.fetching = true;
+      this.loading = true;
+
+      if (newVal === "") {
+        this.handleEmptySearch();
+        return;
+      }
+
+      // Set a new timeout to delay the search function
+      this.timeout = setTimeout(() => {
+        this.fetchSearch();
+      }, 1000);
+    },
+  },
 };
 </script>
