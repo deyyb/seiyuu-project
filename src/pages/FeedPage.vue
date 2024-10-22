fa1315a9c40bcddd265a4ce481ca10939a674079
deyyb <davebajuyo1@gmail.com>
2024-10-22 15:55:20 +0800

    make whats on your mind reactive

--- src/pages/FeedPage.vue
+++ src/pages/FeedPage.vue
@@ -2,60 +2,60 @@
   <q-page padding>
     <!-- content -->
     <div class="flex justify-center">
-      <q-card style="border-radius: 10px">
-        <div class="row flex items-center q-ma-md">
-          <q-avatar size="md" class="q-ma-xs">
-            <img src="icons/fb.svg" />
-          </q-avatar>
-          <q-btn
-            align="left"
-            style="
-              background-color: #e9ebee;
-              color: gray;
-              font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-              font-size: medium;
-              font-weight: 400;
-              width: 450px;
-            "
-            class="q-mx-sm"
-            no-caps
-            flat
-            rounded
-            label="What's on your mind, Rui?"
-          >
-            <template v-slot:prepend>
-              <q-icon name="search" />
-            </template>
-          </q-btn>
-        </div>
+      <div class="col-6">
+        <q-card style="border-radius: 8px; width: 100%">
+          <div class="row flex items-center q-pa-md">
+            <q-avatar size="md" class="q-ma-xs">
+              <img src="icons/fb.svg" />
+            </q-avatar>
+            <div class="col-auto q-mx-sm" style="width: 450px">
+              <q-btn
+                align="left"
+                style="
+                  background-color: #e9ebee;
+                  color: gray;
+                  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                  font-size: medium;
+                  font-weight: 400;
+                  width: 100%;
+                "
+                no-caps
+                flat
+                rounded
+                label="What's on your mind, Rui?"
+              >
+              </q-btn>
+            </div>
+          </div>
 
-        <q-separator class="q-mx-md"></q-separator>
+          <q-separator class="q-mx-md"></q-separator>
 
-        <div class="row q-ma-sm justify-center">
-          <q-btn flat no-caps>
-            <q-icon
-              size="md"
-              name="video_camera_front"
-              color="pink-13"
-            ></q-icon>
-            <div class="h7-label q-mx-sm" style="font-size: 14px">
-              Live Video
-            </div>
-          </q-btn>
-          <q-btn flat no-caps>
-            <q-icon size="md" name="photo_library" color="green-5"></q-icon>
-            <div class="h7-label q-mx-sm" style="font-size: 14px">
-              Photo/Video
-            </div>
-          </q-btn>
-          <q-btn flat no-caps>
-            <q-icon size="md" name="tag_faces" color="amber-5"></q-icon>
-            <div class="h7-label q-mx-sm" style="font-size: 14px">
-              Feelings/Activity
-            </div>
-          </q-btn>
-        </div>
-      </q-card>
+          <div class="row q-pa-sm justify-center">
+            <q-btn class="col-4" flat no-caps>
+              <q-icon
+                size="sm"
+                name="video_camera_front"
+                color="pink-13"
+              ></q-icon>
+              <div class="h7-label q-mx-sm" style="font-size: 12px">
+                Live Video
+              </div>
+            </q-btn>
+            <q-btn class="col-4" flat no-caps>
+              <q-icon size="sm" name="photo_library" color="green-5"></q-icon>
+              <div class="h7-label q-mx-sm" style="font-size: 12px">
+                Photo/Video
+              </div>
+            </q-btn>
+            <q-btn class="col-4" flat no-caps>
+              <q-icon size="sm" name="tag_faces" color="amber-5"></q-icon>
+              <div class="h7-label q-mx-sm" style="font-size: 12px">
+                Feelings/Activity
+              </div>
+            </q-btn>
+          </div>
+        </q-card>
+      </div>
     </div>
   </q-page>
 </template>
