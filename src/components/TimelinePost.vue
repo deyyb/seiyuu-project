64be4e4e801f191331658994e009abd655baf4a3
deyyb <davebajuyo1@gmail.com>
2024-10-23 14:04:54 +0800

    finished timlinepost component

--- src/components/TimelinePost.vue
+++ src/components/TimelinePost.vue
@@ -1,19 +1,19 @@
 <template>
-  <q-card style="border-radius: 8px; width: 100%">
+  <q-card class="q-mb-md" style="border-radius: 8px; width: 100%">
     <div class="row q-pa-md" style="width: 100%">
       <div class="col-auto" style="margin-right: 10px">
         <div class="flex justify-start q-ma-xs">
           <q-avatar size="lg">
-            <img src="icons/kohane.png" />
+            <img :src="iconImg" />
           </q-avatar>
         </div>
       </div>
       <div class="row col flex items-center">
         <div class="col-12 flex items-start">
-          <strong style="color: black; margin: 0 0 0 0">Kohane Azusawa</strong>
+          <strong style="color: black; margin: 0 0 0 0">{{ opName }}</strong>
         </div>
         <div class="flex items-center">
-          <span style="color: gray">Just Now</span>
+          <span style="color: gray">{{ timePassed }}</span>
           <q-avatar size="sm">
             <q-icon name="public" color="blue-grey-13"></q-icon>
           </q-avatar>
@@ -40,9 +40,11 @@
       </div>
     </div>
 
-    <div class="row q-pa-md" style="padding-top: 0">Caption goes here</div>
+    <div class="row q-pa-md" style="padding-top: 0">
+      {{ caption }}
+    </div>
 
-    <q-img src="icons/kohane_card.png"></q-img>
+    <q-img class="q-mb-md" :src="postImg"></q-img>
 
     <q-separator class="q-mx-md"></q-separator>
 
@@ -61,7 +63,7 @@
       </q-btn>
       <q-btn class="col-3" flat no-caps>
         <q-icon size="sm" name="reply" color="blue-grey-11"></q-icon>
-        <div class="h7-label q-mx-sm" style="font-size: 12px">Schare</div>
+        <div class="h7-label q-mx-sm" style="font-size: 12px">Share</div>
       </q-btn>
     </div>
   </q-card>
@@ -69,9 +71,13 @@
 
 <script>
 export default {
-  // name: 'ComponentName',
-  setup() {
-    return {};
+  name: "ComponentName",
+  props: {
+    iconImg: String,
+    opName: String,
+    timePassed: String,
+    caption: String,
+    postImg: String,
   },
 };
 </script>
--- src/pages/FeedPage.vue
+++ src/pages/FeedPage.vue
@@ -1,117 +1,127 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-    <div class="flex justify-center">
-      <div class="col-6">
-        <q-card style="border-radius: 8px; width: 100%">
-          <div class="row flex items-center q-pa-md">
-            <q-avatar size="lg" class="q-ma-xs">
-              <img src="icons/fb.svg" />
-            </q-avatar>
-            <div class="col-auto q-mx-sm" style="width: 450px">
-              <q-btn
-                align="left"
-                style="
-                  background-color: #e9ebee;
-                  color: gray;
-                  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                  font-size: medium;
-                  font-weight: 400;
-                  width: 100%;
-                "
-                no-caps
-                flat
-                rounded
-                label="What's on your mind, Rui?"
-              >
+  <q-scroll-area style="height: 90vh">
+    <q-page padding>
+      <!-- content -->
+      <div class="flex justify-center">
+        <div class="col-6">
+          <q-card style="border-radius: 8px; width: 100%">
+            <div class="row flex items-center q-pa-md">
+              <q-avatar size="lg" class="q-ma-xs">
+                <img src="icons/fb.svg" />
+              </q-avatar>
+              <div class="col-auto q-mx-sm" style="width: 450px">
+                <q-btn
+                  align="left"
+                  style="
+                    background-color: #e9ebee;
+                    color: gray;
+                    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                    font-size: medium;
+                    font-weight: 400;
+                    width: 100%;
+                  "
+                  no-caps
+                  flat
+                  rounded
+                  label="What's on your mind, Rui?"
+                >
+                </q-btn>
+              </div>
+            </div>
+
+            <q-separator class="q-mx-md"></q-separator>
+
+            <div class="row q-pa-sm justify-center">
+              <q-btn class="col-4" flat no-caps>
+                <q-icon
+                  size="sm"
+                  name="video_camera_front"
+                  color="pink-13"
+                ></q-icon>
+                <div class="h7-label q-mx-sm" style="font-size: 12px">
+                  Live Video
+                </div>
+              </q-btn>
+              <q-btn class="col-4" flat no-caps>
+                <q-icon size="sm" name="photo_library" color="green-5"></q-icon>
+                <div class="h7-label q-mx-sm" style="font-size: 12px">
+                  Photo/Video
+                </div>
+              </q-btn>
+              <q-btn class="col-4" flat no-caps>
+                <q-icon size="sm" name="tag_faces" color="amber-5"></q-icon>
+                <div class="h7-label q-mx-sm" style="font-size: 12px">
+                  Feelings/Activity
+                </div>
               </q-btn>
             </div>
-          </div>
+          </q-card>
 
-          <q-separator class="q-mx-md"></q-separator>
-
-          <div class="row q-pa-sm justify-center">
-            <q-btn class="col-4" flat no-caps>
-              <q-icon
-                size="sm"
-                name="video_camera_front"
-                color="pink-13"
-              ></q-icon>
-              <div class="h7-label q-mx-sm" style="font-size: 12px">
-                Live Video
-              </div>
-            </q-btn>
-            <q-btn class="col-4" flat no-caps>
-              <q-icon size="sm" name="photo_library" color="green-5"></q-icon>
-              <div class="h7-label q-mx-sm" style="font-size: 12px">
-                Photo/Video
-              </div>
-            </q-btn>
-            <q-btn class="col-4" flat no-caps>
-              <q-icon size="sm" name="tag_faces" color="amber-5"></q-icon>
-              <div class="h7-label q-mx-sm" style="font-size: 12px">
-                Feelings/Activity
-              </div>
-            </q-btn>
-          </div>
-        </q-card>
-
-        <q-scroll-area style="height: 245px; width: 100%">
-          <!-- CREATE STORY -->
-          <div
-            v-scroll.self="onScroll"
-            class="row no-wrap q-py-md flex"
-            style="max-width: 100%"
-          >
-            <q-card
-              class="my-card text-white q-mr-sm"
-              style="width: 125px; border-radius: 10px"
+          <q-scroll-area style="height: 245px; width: 100%">
+            <!-- CREATE STORY -->
+            <div
+              v-scroll.self="onScroll"
+              class="row no-wrap q-py-md flex"
+              style="max-width: 100%"
             >
-              <div style="max-width: 75px">
-                <q-img
-                  alt="icons/rui.jpg"
-                  src="icons/rui.jpg"
-                  style="width: 125px; height: 210px; border-radius: 10px"
-                >
-                  <div
-                    class="absolute-bottom text-center"
-                    style="
-                      background-color: white;
-                      color: black;
-                      font-weight: 500;
-                    "
+              <q-card
+                class="my-card text-white q-mr-sm"
+                style="width: 125px; border-radius: 10px"
+              >
+                <div style="max-width: 75px">
+                  <q-img
+                    alt="icons/rui.jpg"
+                    src="icons/rui.jpg"
+                    style="width: 125px; height: 210px; border-radius: 10px"
                   >
-                    <div class="q-pa-xs"></div>
-                    <q-avatar
-                      color="white"
-                      style="position: absolute; bottom: 38px; left: 38px"
-                      ><q-icon
-                        size="45px"
-                        color="primary"
-                        name="add_circle"
-                      ></q-icon
-                    ></q-avatar>
-                    <div>Create Story</div>
-                  </div>
-                </q-img>
-              </div>
-            </q-card>
-            <!-- COMPONENTS -->
+                    <div
+                      class="absolute-bottom text-center"
+                      style="
+                        background-color: white;
+                        color: black;
+                        font-weight: 500;
+                      "
+                    >
+                      <div class="q-pa-xs"></div>
+                      <q-avatar
+                        color="white"
+                        style="position: absolute; bottom: 38px; left: 38px"
+                        ><q-icon
+                          size="45px"
+                          color="primary"
+                          name="add_circle"
+                        ></q-icon
+                      ></q-avatar>
+                      <div>Create Story</div>
+                    </div>
+                  </q-img>
+                </div>
+              </q-card>
+              <!-- COMPONENTS -->
 
-            <my-day
-              v-for="myDay in myDays"
-              :key="myDay.myDayName"
-              :my-day-img="myDay.myDayImg"
-              :icon-img="myDay.iconImg"
-              :my-day-name="myDay.myDayName"
-            ></my-day>
-          </div>
-        </q-scroll-area>
+              <my-day
+                v-for="myDay in myDays"
+                :key="myDay.myDayName"
+                :my-day-img="myDay.myDayImg"
+                :icon-img="myDay.iconImg"
+                :my-day-name="myDay.myDayName"
+              ></my-day>
+            </div>
+          </q-scroll-area>
 
-        <timeline-post></timeline-post>
+          <timeline-post
+            v-for="post in posts"
+            :key="post.opName"
+            :caption="post.caption"
+            :icon-img="post.iconImg"
+            :op-name="post.opName"
+            :post-img="post.postImg"
+            :time-passed="post.timePassed"
+          ></timeline-post>
+        </div>
       </div>
-    </div>
-  </q-page>
+    </q-page>
+  </q-scroll-area>
 </template>
 
 <script>
@@ -150,6 +160,22 @@ export default {
           myDayImg: "icons/emu.png",
           myDayName: "Hatsune Miku3",
           iconImg: "icons/hatsune_miku3.jpg",
+        },
+      ],
+      posts: [
+        {
+          iconImg: "icons/kohane.png",
+          opName: "Kohane Azusawa",
+          timePassed: "Just Now",
+          caption: "よろしくお願いします",
+          postImg: "icons/kohane_card.png",
+        },
+        {
+          iconImg: "icons/akito.jpg",
+          opName: "Akito Shinonome",
+          timePassed: "Just Now",
+          caption: "HELP WTF IS THIS CREATURE DOING IN FRONT OF MY HOUSE",
+          postImg: "icons/EMU.jpg",
         },
       ],
     };
